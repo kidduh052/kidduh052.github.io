09780437c535f4ec05cfd6254e3b22e0881dd61e
kidduh052 <118465788+kidduh052@users.noreply.github.com>
2022-11-17 13:13:21 -0300

    Add files via upload

---
+++ clique.js
@@ -0,0 +1,10 @@
+function umClique(){
+  document.getElementById("meliodas").src="6536cedc18e1f5f5720ab7b5af4a2fb7.gif";
+  
+}
+
+
+function doisCliques(){
+  document.getElementById("meliodas").src="meliodasdemonhealingpower.gif";
+  
+}
